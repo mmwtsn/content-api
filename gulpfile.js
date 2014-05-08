9c787b0bb6990bea3a6f2e5f76e7aa31cf397aa4
Maxwell Watson <maxwell.watson@ogilvy.com>
2014-05-08 14:05:19 -0400

    uses gulp as front-end task runner

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+/**
+ *
+ * Gulp.js! A task runner.
+ *
+ */
+
+// Require Gulp
+var gulp = require('gulp');
+
+// Require our plug-ins
+var sass   = require('gulp-sass');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// Define asset paths
+var assetsPath = './public/assets';
+var sassPath   = assetsPath + '/scss/*.scss';
+var cssPath    = assetsPath + '/css';
+var jsPath     = assetsPath + '/js/*.js';
+
+// TODO - Add these!
+// gulp-imagemin
+// gulp-minify-css
+// gulp-uglify
+// gulp-jslint
+// gulp-scss-lint
+// gulp-lintspaces
+
+// JavaScript lint task
+gulp.task('lint', function() {
+  return gulp.src(jsPath)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// SASS compilation task
+gulp.task('sass', function() {
+  return gulp.src(sassPath)
+    .pipe(sass())
+    .pipe(gulp.dest(cssPath));
+});
+
+// Watch files for changes
+gulp.task('watch', function() {
+  gulp.watch(sassPath, ['sass']);
+  gulp.watch(jsPath, ['lint']);
+});
+
+// Default task definition
+gulp.task('default', ['lint', 'sass', 'watch']);
