0bb8b00af443541d72eaf33a6a66834bc1fb9b5d
Maxwell Watson <maxwell.watson@ogilvy.com>
2014-05-08 17:15:37 -0400

    defines users factory

--- spec/features/user_starts_spec.rb
+++ spec/features/user_starts_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 
-feature 'User can create an account' do
-  scenario 'Viewing the app' do
-    visit '/'
+feature 'user can get started' do
+  scenario 'viewing the app' do
+    visit root_path
 
     expect(page).to have_content('content api')
+  end
+
+  scenario 'create account' do
+    visit root_path
+    
+    fill_in 'username', with: 'user@ibmcloud.io'
+    fill_in 'password', with: 'password'
+    fill_in 'confirmation', with: 'password'
+    click_button 'create_account'
   end
 end
