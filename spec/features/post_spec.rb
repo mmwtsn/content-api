83a062f7611138a84226dd423d5f100c5bc399d1
Maxwell Watson <maxwell.watson@ogilvy.com>
2014-05-12 13:27:50 -0400

    stubs integration tests for post resources

---
+++ spec/features/post_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+feature 'user can' do
+  scenario 'create a post' do
+    pending
+  end
+
+  scenario 'view all posts' do
+    pending
+  end
+
+  scenario 'edit a post' do
+    pending
+  end
+
+  scenario 'delete a post' do
+    pending
+  end
+end
