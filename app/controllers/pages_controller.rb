733623c44c68d74619b3c28fe8f7b6caca75fd57
Maxwell Watson <maxwell.watson@ogilvy.com>
2014-05-12 15:32:33 -0400

    pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,16 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
+
+  def new
+    @page = Page.new
+  end
   
   def show
+    @pages = Page.all
+  end
+
+  def create
+    Page.create!(params[:page].permit(:title, :body))
+    redirect_to pages_path
   end
 end
