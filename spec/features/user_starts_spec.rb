6ff2ac8716162626b139fb4b4262589852aaa800
Maxwell Watson <maxwell.watson@ogilvy.com>
2014-05-12 13:20:31 -0400

    updates to initial user feature

--- spec/features/user_starts_spec.rb
+++ spec/features/user_starts_spec.rb
@@ -1,18 +1,29 @@
 require 'spec_helper'
 
-feature 'user can get started' do
-  scenario 'viewing the app' do
+feature 'anyone can' do
+  scenario 'view the app' do
     visit root_path
 
     expect(page).to have_content('content api')
   end
+end
 
-  scenario 'create account' do
+feature 'user can' do
+  scenario 'create an account, sign in and sign out' do
     visit root_path
     
-    fill_in 'username', with: 'user@ibmcloud.io'
-    fill_in 'password', with: 'password'
-    fill_in 'confirmation', with: 'password'
-    click_button 'create_account'
+    # For the initial test, manually seed the user information
+    click_link 'register'
+    fill_in 'user_email', with: 'user@ibmcloud.io'
+    fill_in 'user_password', with: 'password'
+    fill_in 'user_password_confirmation', with: 'password'
+
+    # register and sign in
+    click_button 'sign up'
+    expect(page).to have_content('authenticated')
+
+    # sign out
+    click_link 'sign out'
+    expect(page).to have_content('unauthenticated')
   end
 end
