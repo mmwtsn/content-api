db8121770666bf35465539271dd6ae4d3746b4fb
Maxwell Watson <maxwell.watson@ogilvy.com>
2014-05-13 15:31:19 -0400

    adds and updates index/show/edit methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,35 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @pages = Page.all
+  end
+
   def new
     @page = Page.new
-  end
-  
-  def show
-    @pages = Page.all
   end
 
   def create
     Page.create!(page_params)
     redirect_to pages_path
+  end
+
+  def show
+    @page = Page.find(params[:id])
+  end
+
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+
+    if @page.update(page_params)
+      redirect_to @page
+    else
+      render 'edit'
+    end
   end
 
   private
