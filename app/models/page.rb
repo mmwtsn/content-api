2b99bd84c9fbbb6c1c90d8473d3bc5fecafda025
Maxwell Watson <maxwell.watson@ogilvy.com>
2014-05-19 15:28:47 -0400

    swaps default image with local version

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,7 @@
 class Page < ActiveRecord::Base
   has_attached_file :header, 
                     :styles => { :default => '1900x500' },
-                    :default_url => 'http://openclipart.lynms.edu.hk/clipart/Clue/Clue_Simple_Clouds.svg'
+                    :default_url => '/images/default-header.png'
 
   validates_attachment_content_type :header, :content_type => /\Aimage\/.*\Z/
 end
