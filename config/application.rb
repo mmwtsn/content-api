ef18cfdfa43af1c31aee91cd36c1c6eb2c830ec6
Maxwell Watson <maxwell.watson@ogilvy.com>
2014-05-20 15:18:46 -0400

    disables sprockets asset handling

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ module ContentApi
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Disable Sprockets so we can use Gulp to manage front-end assets
+    config.assets.enabled = false
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
