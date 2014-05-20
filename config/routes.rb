6086c503a0840621101ba6e4b8a3c8ca51ebcb4c
Maxwell Watson <maxwell.watson@ogilvy.com>
2014-05-20 16:54:08 -0400

    hard-codes route to test solutions template

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  # Temporary hack around our hackity-hack rendering
+  # TODO - set up proper slug-based routing
+  get '/cloud-computing/us/en/solutions-gaming.html', to: 'pages#show', id: 1
+
   resources :pages
 
   devise_for :users
