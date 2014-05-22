25213852b05f49f7218da9732db4ac30019e511c
Maxwell Watson <maxwell.watson@ogilvy.com>
2014-05-22 10:53:06 -0400

    adds :published field to permitted params

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -36,7 +36,7 @@ class PagesController < ApplicationController
 
   private
   def page_params
-    params[:page].permit(:title, :body, :header)
+    params[:page].permit(:title, :body, :header, :published)
   end
 
   def get_requested_page
