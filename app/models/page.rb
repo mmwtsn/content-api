9263e3f2557a1945ecba04a507374453a3f19ff5
M. Maxwell Watson <hi@mmwtsn.com>
2014-05-22 10:51:16 -0400

    conditial validation for "published" state & documentation

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,16 @@
 class Page < ActiveRecord::Base
+  # Paperclip file configuration
   has_attached_file :header, 
                     :styles => { :default => '1900x500' },
                     :default_url => '/images/default-header.png'
 
+  # Paperclip validation helper method to ensure header image type is valid
   validates_attachment_content_type :header, :content_type => /\Aimage\/.*\Z/
+
+  # Require all fields if the user attempts to publish the page
+  with_options if: :published? do |published_page|
+    published_page.validates :header, presence: true
+    published_page.validates :title,  presence: true
+    published_page.validates :body,   presence: true
+  end
 end
