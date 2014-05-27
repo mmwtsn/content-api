129c123e7b73a5c07081fca34c4002c16d16d2a4
Maxwell Watson <maxwell.watson@ogilvy.com>
2014-05-27 17:07:18 -0400

    temporary view helpers for gaming

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def user_requested?(controller, action, id)
+    if params[:controller] == controller && params[:action] == action && params[:id] == id
+      true
+    end
+  end
+
+  def gaming_scenario?
+    if user_requested?('pages', 'show', 1)
+      true
+    end
+  end
 end
