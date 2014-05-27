aeac4344982244486e9cef2ff398427d3559f899
M. Maxwell Watson <hi@mmwtsn.com>
2014-05-27 16:37:45 -0400

    miserable hack to fix broken layout

---
+++ public/assets/js/hacks.js
@@ -0,0 +1,8 @@
+$(window).load(function() {
+  //
+  // Stupid hacks to remedy a very broken page once everything has executed
+  //
+  $('.ibm-band.ibm_cci-solutions-gaming.section1').attr('id', 'ibm-solutions-gaming-hero');
+  $('.ibm-band.ibm_cci-solutions-gaming.section2').attr('id', 'ibm-solutions-gaming-intro');
+  $('.ibm-band.ibm_cci-solutions-gaming.section3').attr('id', 'ibm-solutions-gaming-header');
+});
