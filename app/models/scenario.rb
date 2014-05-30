9f2c1f089f53b9a953c7d6e4ee2e79b4033e5096
M. Maxwell Watson <hi@mmwtsn.com>
2014-05-30 16:03:37 -0400

    adds paperclip config. info. for avatar image

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -1,3 +1,11 @@
 class Scenario < ActiveRecord::Base
   belongs_to :page
+
+  # Paperclip file configuration
+  has_attached_file :avatar,
+                    :styles => { :default => '175x175' },
+                    :default_url => '/public/assets/images/defaults/avatar.png'
+
+  # Paperclip validation helper method to ensure header image type is valid
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
