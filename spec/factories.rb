ddfc800dd625a9e2990399fd54057a0f5b401e5d
Maxwell Watson <maxwell.watson@ogilvy.com>
2014-05-30 16:00:53 -0400

    manually sets image factory values as suggested by pivotal labs

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   # Simple user factory for Devise
   factory :user do
-    email 'user@ibmcloud.io'
+    sequence(:email) { |n| "user_#{n}@ibmcloud.io" }
     password 'password'
     password_confirmation 'password'
   end
@@ -9,12 +9,28 @@ FactoryGirl.define do
   factory :page do
     title 'Buying a Bicycle'
     body 'Today I will buy a bicycle.'
+
+    # Allow header image to be tested only when necessary
+    factory :post_with_header do
+      header_file_name    { 'header.jpg' }
+      header_file_size    { 269410 }
+      header_content_type { 'image/jpeg' }
+      header_updated_at   { Time.now }
+    end
   end
 
   factory :scenario do
     quote 'I want to globally globalize global globes'
     pitch 'High-performance geo-buzzword'
     page # associate :scenario factory with :page factory
+
+    # Allow avatar image to be tested only when necessary
+    factory :scenario_with_avatar do
+      avatar_file_name    { 'avatar.png' }
+      avatar_file_size    { 81581 }
+      avatar_content_type { 'image/png' }
+      avatar_updated_at   { Time.now }
+    end
   end
 
   # Allow for user to specify the number of associated :scenario factories
