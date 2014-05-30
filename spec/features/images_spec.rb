476a52130df5eff44579da7d335e0cf02fdfe9c4
M. Maxwell Watson <hi@mmwtsn.com>
2014-05-30 13:51:37 -0400

    stubbed images spec

---
+++ spec/features/images_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+# Get access to Capybara's "sign_in" method
+include Warden::Test::Helpers
+Warden.test_mode!
+
+feature 'user can' do
+
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+    login_as(@user)
+  end
+
+  scenario 'add a page header' do
+    pending
+  end
+
+  scenario 'edit a page header' do
+    pending
+  end
+
+  scenario 'add a scenario avatar' do
+    pending
+  end
+
+  scenario 'edit a scenario avatar' do
+    pending
+  end
+end
