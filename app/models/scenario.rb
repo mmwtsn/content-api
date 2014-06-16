db9e62ded934c1e1a5c9b4ce50cf54d570a9f3c6
Maxwell Watson <maxwell.watson@ogilvy.com>
2014-06-16 14:25:31 -0400

    corrents erroneous avatar file path

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -5,7 +5,7 @@ class Scenario < ActiveRecord::Base
   # Paperclip file configuration
   has_attached_file :avatar,
                     :styles => { :default => '175x175' },
-                    :default_url => '/public/assets/images/defaults/avatar.png'
+                    :default_url => '/assets/images/defaults/avatar.png'
 
   # Paperclip validation helper method to ensure header image type is valid
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
