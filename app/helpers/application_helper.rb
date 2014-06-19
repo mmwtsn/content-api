7929e1fef838ddbf21f08d5fc7bfec5b179f3a93
M. Maxwell Watson <hi@mmwtsn.com>
2014-06-19 13:07:13 -0400

    helper method for formatting relative icon_url paths

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,14 @@ module ApplicationHelper
       true
     end
   end
+
+  def format_icon_url(url)
+    if url =~ /http/
+      sanitized_url = url
+    else
+      sanitized_url = "http://ibm.com#{url}"
+    end
+
+    return sanitized_url
+  end
 end
