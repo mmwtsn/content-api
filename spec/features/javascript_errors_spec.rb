2e1db8c9cf759cb3b2dd357318714451db300d83
M. Maxwell Watson <hi@mmwtsn.com>
2014-06-20 16:39:23 -0400

    refactors "js: true" up to feature level

--- spec/features/javascript_errors_spec.rb
+++ spec/features/javascript_errors_spec.rb
@@ -1,34 +1,34 @@
 require 'spec_helper'
 
-feature 'JavaScript (un-authed)' do
-  scenario 'welcome#index has no errors', js: true do
+feature 'JavaScript (un-authed)', js: true do
+  scenario 'welcome#index has no errors' do
     visit(root_path)
     expect(page).not_to have_errors
   end
 end
 
-feature 'JavaScript (authed)' do
+feature 'JavaScript (authed)', js: true do
   before(:each) do
     auth_user
     @page = FactoryGirl.create(:page, :with_scenarios, :with_resources)
   end
 
-  scenario 'welcome#index has no errors', js: true do
+  scenario 'welcome#index has no errors' do
     visit(root_path)
     expect(page).not_to have_errors
   end
 
-  scenario 'pages#index has no errors', js: true do
+  scenario 'pages#index has no errors' do
     visit pages_path
     expect(page).not_to have_errors
   end
 
-  scenario 'pages#show has no errors', js: true do
+  scenario 'pages#show has no errors' do
     visit page_path(@page)
     expect(page).not_to have_errors
   end
 
-  scenario 'scenarios#show has no errors', js: true do
+  scenario 'scenarios#show has no errors' do
     visit scenario_path(@page.scenarios.first)
     expect(page).not_to have_errors
   end
