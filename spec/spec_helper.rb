1007e1b47a8202119a80586c1505136c2db43cc5
Maxwell Watson <maxwell.watson@ogilvy.com>
2014-06-23 17:37:23 -0400

    runs specs sequentially to preserve sanity

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,7 +39,7 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = "random"
+  #config.order = "random"
 
   # Make FactoryGirl's methods available
   config.include FactoryGirl::Syntax::Methods
