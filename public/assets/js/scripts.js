3e51d14de5f98c1ddd6d955dbca8e966cccc0640
Maxwell Watson <maxwell.watson@ogilvy.com>
2014-06-23 17:42:02 -0400

    adds error messages and clears search results between queries

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,2 +1,27 @@
 $(document).ready(function() {
+  //
+  // Ensure search input field is empty on load
+  //
+  var $product = $('#product');
+  $product.val('');
+
+  //
+  // Prevent user from submitting empty search queries
+  //
+  $('#search input[type="submit"]').on('click', function(e) {
+    var query = $product.val();
+    var $results = $('.results');
+
+    // Ensure results and errors are cleared between searches
+    $results.empty();
+    $('.error').slideUp();
+
+    // TODO
+    // Display AJAX loading GIF while request is being processed
+
+    if (query == '') {
+      e.preventDefault();
+      $('main').prepend('<p class="error">Search query cannot be blank!</p>');
+    }
+  });
 });
