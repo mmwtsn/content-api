e34024b1c717fd81b945fe1eb97526b5801d9451
M. Maxwell Watson <hi@mmwtsn.com>
2014-07-02 17:21:27 -0400

    adds preview test

---
+++ spec/features/preview_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+feature 'Scenario Products' do
+  before(:each) do
+    auth_user
+
+    @page     = FactoryGirl.create(:page, :with_everything)
+    @scenario = @page.scenarios.first
+    @resource = @page.resources.first
+    @product  = @scenario.products.first
+  end
+  
+  # User can preview the published state of any page listed on pages#index
+  scenario 'preview from pages#index' do
+    visit pages_path
+    first(:link, 'preview').click
+
+    save_and_open_page
+    expect_all_content
+  end
+  
+  # User can preview the published state of any individual page view
+  scenario 'preview from pages#show' do
+    visit page_path(@page)
+
+    # TODO
+    # Ensure preview assets are loaded
+    # Ensure admin assets are not loaded
+
+    save_and_open_page
+    expect_all_content
+  end
+
+  # Helper method to ensure all pages#preview specs represent every model
+  def expect_all_content
+    expect(page).to have_content(@page.title)
+    expect(page).to have_content(@scenario.pitch)
+    expect(page).to have_content(@resource.body)
+    expect(page).to have_content(@product.name)
+  end
+end
