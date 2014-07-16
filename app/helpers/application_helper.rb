d737ed2c0713e692c947835baaa050f17ac1f90e
Maxwell Watson <maxwell.watson@ogilvy.com>
2014-07-16 16:43:14 -0400

    removes admin? and preview? helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,4 @@
 module ApplicationHelper
-  def user_requested?(controller, action, id)
-    if params[:controller] == controller && params[:action] == action && params[:id] == id
-      true
-    end
-  end
-  def preview
-    if params[:controller] == 'pages' && params[:action] == 'preview'
-      true
-    end
-  end
-
-  def admin
-    unless preview
-      true
-    end
-  end
 
   def format_icon_url(url)
     if url =~ /http/
@@ -25,4 +9,5 @@ module ApplicationHelper
 
     return sanitized_url
   end
+
 end
