0346d6c8443aff3920ecf440e5e7c86f12515a2a
M. Maxwell Watson <hi@mmwtsn.com>
2014-07-17 15:18:41 -0400

    allows anonymous users to see #preview

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:preview]
   before_action :get_requested_page, only: [:show, :preview, :edit, :update, :destroy]
 
   def index
