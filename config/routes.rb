64bbedbeb942bf17035e00ee0c15200e02471b22
Maxwell Watson <maxwell.watson@ogilvy.com>
2014-07-21 15:03:34 -0400

    adds todo note for questionable routing voodoo

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,12 @@ Rails.application.routes.draw do
     resources :resources, only: [:create, :edit, :update, :destroy]
   end
 
+  #
+  # TODO - How else to avoid deeply nested routes without re-generating
+  #        these Scenarios resources? Passing an empty array to only
+  #        ensures that no new resources are generated as we've already
+  #        generated everything we need above (with pages/scenarios)
+  #
   resources :scenarios, only: [] do
     resources :products, only: [:search, :create, :destroy]
   end
