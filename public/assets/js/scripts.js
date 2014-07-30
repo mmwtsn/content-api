97299f5d6406080a69e09d12b677a93627bd4cd9
M. Maxwell Watson <hi@mmwtsn.com>
2014-07-30 16:03:45 -0400

    adds resources#create module with config, init()

---
+++ public/assets/js/scripts.js
@@ -0,0 +1,21 @@
+//
+// Handles the displaying of and submission of AJAX-ready #create forms
+//
+
+// Define Resource#create module
+var create = (function() {
+
+  // Define config object for later use
+  var config = {};
+
+  // Initialize instance of module, calls setup()
+  var init = function( resource ) {
+  };
+
+  // Expose public methods through create module
+  return {
+    init: init,
+    config: config
+  };
+
+})();
