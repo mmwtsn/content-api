0a760364edb272d5eea7e896dea984f8d701c613
Maxwell Watson <maxwell.watson@ogilvy.com>
2014-07-30 17:08:45 -0400

    adds toggle()

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -11,6 +11,8 @@ var create = (function() {
   // Initialize instance of module, calls setup()
   var init = function( resource ) {
     config = configure( resource );
+
+    config.$show.on( 'click', toggle );
   };
 
   // Build config object with jQuery obects and resources string
@@ -19,6 +21,7 @@ var create = (function() {
 
     // Use singular form of resource for selector
     resource = singularize_resource( resource );
+
     config['$submit']  = $('#create_' + resource);
     config['$show']    = $('#show_new_' + resource);
     config['$form']    = $('#new_' + resource);
@@ -29,6 +32,31 @@ var create = (function() {
   // Strip last character from plural resource
   var singularize_resource = function( resource ) {
     return resource.slice(0, -1);
+  };
+
+  // Toggle visibility of $form if hidden
+  var toggle = function() {
+
+    // Cache references for readability
+    var $form   = config.$form;
+    var $submit = config.$submit;
+
+    // Check visibility of $form TODO - extract
+    if( !$form.hasClass('visible') ) {
+
+      // Move $form into position
+      $form.insertBefore( $submit );
+
+      // Update visibility and state
+      $form
+        .slideDown( 600 )
+        .addClass('visible');
+
+    }
+
+    // Ensure nothing happens when link or button is clicked
+    return false;
+
   };
 
   // Expose public methods through create module
