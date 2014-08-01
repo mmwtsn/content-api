1924c17f5a77b366f27c0de8cf2b6728d410c887
M. Maxwell Watson <hi@mmwtsn.com>
2014-08-01 13:45:31 -0400

    adds submit() and its private dependencies

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -13,6 +13,7 @@ var create = (function() {
     config = configure( resource );
 
     config.$show.on( 'click', toggle );
+    config.$submit.on( 'click', submit );
   };
 
   // Build config object with jQuery obects and resources string
@@ -76,7 +77,61 @@ var create = (function() {
 
   };
 
-  // Expose public methods through create module
+  // Attempt to submit the create resource form
+  var submit = function() {
+    return form_is_complete() ? ajax_submit() : submit_error();
+  };
+
+  // Checks to see if the create resource form is complete
+  var form_is_complete = function() {
+    var $input = config.$form.children( 'input[type="text"]' ).first();
+    var value  = $input.val();
+
+    return (value !== '');
+  };
+
+  // Displays an error to the user if the form is submitted prematurely
+  var submit_error = function() {
+    var error = '<p class="error">Whoa! Submit something.</p>';
+
+    config.$form.prepend( error );
+
+    return false;
+  };
+
+  // Submit the create resource form via an AJAX POST request
+  var ajax_submit = function() {
+
+    // Build submit path for current nested Rails resource
+    var url = build_nested_path();
+
+    // Build POST data from form
+    var data = config.$form.serialize();
+
+    // Submit via AJAX to save record
+    $.post(url, data);
+
+    // Clear submitted form input
+    reset_form();
+
+    // Ensure submit event is not triggered
+    return false;
+
+  };
+
+  // Return the correct Rails RESTful resource path
+  var build_nested_path = function() {
+    var id = window.location.pathname;
+    var url = id + '/' + config.resource;
+
+    return url;
+  };
+
+  // Reset form to empty state upon successful POST
+  var reset_form = function() {
+    config.$form.children('input[type="text"]').val('');
+  };
+
   return {
     init: init,
     config: config
