da3764b38b3cfc31ade2216a838402c535b6d0bc
M. Maxwell Watson <hi@mmwtsn.com>
2014-08-04 13:27:28 -0400

    breaks scripts.js into smaller module files

---
+++ public/assets/js/init.js
@@ -0,0 +1,7 @@
+// Initialize toggle module for Scenarios and Resources controllers
+var toggle_scenario = toggle().init( 'scenarios');
+var toggle_resource = toggle().init( 'resources' );
+
+// Initialize create module for Scenarios and Resources controllers
+var create_scenario = create().init( 'scenarios ');
+var create_resource = create().init( 'resources' );
