967560a627dc542286667aa1373d18e8019acfdf
M. Maxwell Watson <hi@mmwtsn.com>
2014-08-11 11:25:58 -0400

    removes extra space typo

--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -1,7 +1,7 @@
 // Initialize toggle module for Scenarios and Resources controllers
-var toggle_scenario = toggle().init( 'scenarios');
 var toggle_resource = toggle().init( 'resources' );
+var toggle_scenario = toggle().init( 'scenarios');
 
 // Initialize create module for Scenarios and Resources controllers
-var create_scenario = create().init( 'scenarios ');
 var create_resource = create().init( 'resources' );
+var create_scenario = create().init( 'scenarios' );
