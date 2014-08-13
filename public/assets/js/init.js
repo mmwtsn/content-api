97747a5abb72f2d66f44e2bca4e65dfa877a8870
mmwtsn <mwatson@dosomething.org>
2014-08-13 16:14:27 -0400

    removes call to clear.js

--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -5,6 +5,3 @@ var toggle_scenario = toggle().init( 'scenarios');
 // Initialize create module for Scenarios and Resources controllers
 var create_resource = create().init( 'resources' );
 var create_scenario = create().init( 'scenarios' );
-
-// Ensure inputs are cleared on load
-var clear_inputs = clear();
