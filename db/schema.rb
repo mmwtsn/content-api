f2d7dba5419b919ec4ddf64d82e3a55a09f563a6
M. Maxwell Watson <hi@mmwtsn.com>
2014-08-13 16:13:00 -0400

    adds friendly_ids to scenarios

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140813181504) do
+ActiveRecord::Schema.define(version: 20140813193639) do
 
   create_table "pages", force: true do |t|
     t.string   "title"
@@ -60,9 +60,11 @@ ActiveRecord::Schema.define(version: 20140813181504) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
 
   add_index "scenarios", ["page_id"], name: "index_scenarios_on_page_id", using: :btree
+  add_index "scenarios", ["slug"], name: "index_scenarios_on_slug", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
