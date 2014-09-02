4749db11856011721d732e72e2f489da92e67dea
M. Maxwell Watson <hi@mmwtsn.com>
2014-09-02 17:25:50 -0400

    adds ajax:success binding for .delete-product

--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -5,3 +5,19 @@
 // Initialize create module for Scenarios and Resources controllers
 //var create_resource = create().init( 'resources' );
 //var create_scenario = create().init( 'scenarios' );
+
+$('.delete-product').bind('ajax:success', function() {
+
+  // Remove closest node with "resource" class
+  $(this).parents('.product').remove();
+
+  // If user just deleted the last resource, don't blow a hole in the page
+  if (!$('.products .product').length) {
+    // TODO - abstract this into a partial
+    $('.products').append(
+      '<section class="empty">'
+    +   '<p>no saved products&#133;</p>'
+    + '</section>'
+    );
+  }
+});
